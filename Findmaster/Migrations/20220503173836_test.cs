7555fb493054a61b008b343c140529e25bd647d8
Imran Khaider <201604@astanait.edu.kz>
2022-05-04 09:32:54 +0600

    Migration Fixes

---
+++ Findmaster/Migrations/20220503173836_test.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Findmaster.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Favourites",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "VacancyId",
+                table: "Favourites",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Applications",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "VacancyId",
+                table: "Applications",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Favourites");
+
+            migrationBuilder.DropColumn(
+                name: "VacancyId",
+                table: "Favourites");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Applications");
+
+            migrationBuilder.DropColumn(
+                name: "VacancyId",
+                table: "Applications");
+        }
+    }
+}
